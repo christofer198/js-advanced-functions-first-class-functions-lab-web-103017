97491124b1097742dad7f7fa0c54ad6c4fc4d2ee
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-12 15:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const returnFirstTwoDrivers = function(drivers){
   return drivers.slice(0,2)
 }
 
-const
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(drivers.length-2, drivers.length)
+}
