39356b92d47dab34181a81608985d0c8d1c76836
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-12-12 15:45:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers(drivers){
-  const newArray = []
-  for(i = 0; i < 2; i++){
-    newArray.push(drivers[i])
-  }
-  return newArray
+const returnFirstTwoDrivers = function(drivers){
+  return drivers.slice(0,2)
 }
